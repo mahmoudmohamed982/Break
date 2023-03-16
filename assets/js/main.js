2b6cb1b5bd8b5c20f8edd227084e62d26d37170c
mahmoud mohamed <mahmoudmohamed.2m.404@gmail.com>
2023-03-16 22:08:36 +0200

    upload edits

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,64 +1,54 @@
-$(document).ready(function () {
-  $(".services-carousel").owlCarousel({
-    loop: true,
-    margin: 10,
-    rtl: true,
-    responsiveClass: true,
-    responsive: {
-      0: {
-        items: 1,
-        nav: true,
-        margin: 20,
-      },
-      600: {
-        items: 2,
-        nav: true,
-        margin: 30,
-      },
-      1000: {
-        items: 3,
-        nav: true,
-        margin: 25,
-      },
-    },
-  });
-});
-
-$(document).ready(function () {
-  $(".customers-carousel").owlCarousel({
-    loop: true,
-    margin: 10,
-    rtl: true,
-    responsiveClass: true,
-    responsive: {
-      0: {
-        items: 2,
-        nav: true,
-        margin: 20,
-      },
-      600: {
-        items: 3,
-        nav: true,
-        margin: 30,
-      },
-      1000: {
-        items: 4,
-        nav: true,
-        margin: 25,
-      },
-    },
-  });
-});
-
-function toggleForm(){
-  document.body.classList.toggle('activeForm');
+function toggleProfile() {
+  
+  document.getElementById("myOrdersLink").classList.remove('active-user');
+  document.getElementById("cashBackLink").classList.remove('active-user');
+  document.getElementById("myDataLink").classList.add('active-user');
+  document.getElementById("myOrders").classList.add('d-none');
+  document.getElementById("cashBack").classList.add('d-none');
+  document.getElementById("myData").classList.remove('d-none');
 }
-var data=0;
-function increment(){
-  data=data+1;
-  document.getElementById("root").innerText=data;
+
+function toggleMyOrders() {
+  document.getElementById("myDataLink").classList.remove('active-user');
+  document.getElementById("cashBackLink").classList.remove('active-user');
+  document.getElementById("myOrdersLink").classList.add('active-user');
+  document.getElementById("myData").classList.add('d-none');
+  document.getElementById("cashBack").classList.add('d-none');
+  document.getElementById("myOrders").classList.remove('d-none');
+
 }
-function decrement(){
-  data=data-1;
-  document.getElementById("root").innerText=data;
+function toggleCashBack() {
+  document.getElementById("myOrdersLink").classList.remove('active-user');
+  document.getElementById("myDataLink").classList.remove('active-user');
+  document.getElementById("cashBackLink").classList.add('active-user');
+  document.getElementById("myOrders").classList.add('d-none');
+  document.getElementById("myData").classList.add('d-none');
+  document.getElementById("cashBack").classList.remove('d-none');
+
+}
+
+function newOrders() {
+  
+  document.getElementById("OrderHistoryBtn").classList.remove('active-order');
+  document.getElementById("NewOrdersBtn").classList.add('active-order');
+  document.getElementById("OrderHistory").classList.add('d-none');
+  document.getElementById("NewOrders").classList.remove('d-none');
+}
+
+function ordersHistory() {
+  document.getElementById("NewOrdersBtn").classList.remove('active-order');
+  document.getElementById("OrderHistoryBtn").classList.add('active-order');
+  document.getElementById("NewOrders").classList.add('d-none');
+  document.getElementById("OrderHistory").classList.remove('d-none');
+
+}
+
+var data = 0;
+function increment() {
+  data = data + 1;
+  document.getElementById("root").innerText = data;
+}
+function decrement() {
+  data = data - 1;
+  document.getElementById("root").innerText = data;
 }
